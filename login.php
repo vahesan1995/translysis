cec7761a6cc825c261c6642a4b296a772850f7ae
vahesan1995 <vahesan.15@cse.mrt.ac.lk>
2018-05-17 03:56:43 +0530

    small changes

--- login.php
+++ login.php
@@ -1,12 +1,12 @@
 <?php 
 include_once("config.php");
-session_start();
+session_start();        //start session
 $_SESSION['user_login_status'] = "alreadylogedin";
-$username = $_POST['username'];
-$password=$_POST['password'];
-$password = md5($password);
+$username = $_POST['username'];     //get username
+$password=$_POST['password'];       //get password
+$password = md5($password);         //encryption
 
-$results = $mysqli->query("SELECT * from user where username = '".$username."' and password = '".$password."'");
+$results = $mysqli->query("SELECT * from user where username = '".$username."' and password = '".$password."'");    //user details
 $num_rows = mysqli_num_rows($results);
 if ($num_rows==1) {
     $obj = $results->fetch_object();
@@ -15,6 +15,7 @@ if ($num_rows==1) {
     $_SESSION['sessionStartTime'] = time();
     $_SESSION['username'] = $obj->username;
 
+    //redirect to home
     print('<script> window.location="home.php"; </script> ');
 }else {
     if(isset($_SESSION['user_login_status'])) {
@@ -22,7 +23,7 @@ if ($num_rows==1) {
         unset($_SESSION['user_login_id']);
         unset($_SESSION['indexNo']);
     }
-    mysqli_close($mysqli);
+    mysqli_close($mysqli);      //close database connection
     $error_msg=md5("wrongUsernamePassword");
     redirect("index.php?error=".$error_msg);
 }
