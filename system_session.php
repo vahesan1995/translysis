b8b342bb94ebd80f76d3f59668f6dac3b9649f53
vahesan1995 <vahesan.15@cse.mrt.ac.lk>
2018-05-17 04:23:49 +0530

    small changes

--- system_session.php
+++ system_session.php
@@ -1,15 +1,15 @@
 <?php
 ob_start();
 if(session_id() == '') {
-    session_start();
+    session_start();    //start session
 }
  
 if(isset($_SESSION['user_login_status'])){
-	if($_SESSION['user_login_status']!='alreadylogedin'){
+	if($_SESSION['user_login_status']!='alreadylogedin'){   //not logged in
 		header("location:http://localhost/translysis/index.php?msg=Please Login First");
 	}
 }
-else {
+else {  //logged in
     print('<script> window.location="index.php?msg=Please Login First"; </script> ');
 }
 
