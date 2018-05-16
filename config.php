ce050b513b2aad305009552ede7c4e0c88c7fc4d
vahesan1995 <vahesan.15@cse.mrt.ac.lk>
2018-05-17 03:22:35 +0530

    download icon added

--- config.php
+++ config.php
@@ -1,16 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Sangeerththan
- * Date: 3/11/2017
- * Time: 6:33 PM
- */
 
 $db_username = 'root';
 $db_password = '';
 $db_name = 'translysis';
 $db_host = 'localhost';
 
+//creating a connection with database
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 
 ?>
