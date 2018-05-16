235401dd048181d7eba7fe1c124e45b907ec2cd3
vahesan1995 <vahesan.15@cse.mrt.ac.lk>
2018-05-17 03:49:05 +0530

    small changes

--- hidenotification.php
+++ hidenotification.php
@@ -1,7 +1,8 @@
 <?php
 include_once("config.php");
 include_once ("system_session.php");
-$username = $_SESSION['username'];
-$query=$mysqli->query("Update files set notification=0 WHERE username='$username'");
+$username = $_SESSION['username'];      //username of current user
+$query=$mysqli->query("Update files set notification=0 WHERE username='$username'");    //changing notification flag
 
+//redirect
 echo "<script language='javascript'> window.location = 'viewFiles.php'</script>";
