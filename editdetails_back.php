4030f8738b27172120f3fb8dd097eb4de871ef64
vahesan1995 <vahesan.15@cse.mrt.ac.lk>
2018-05-17 03:47:03 +0530

    small changes

--- editdetails_back.php
+++ editdetails_back.php
@@ -3,33 +3,33 @@ include_once("config.php");
 include_once ("system_session.php");
 $username = $_SESSION['username'];
 
-error_reporting(E_ALL);
+error_reporting(E_ALL);     //reporting errors regarding inputs
 ini_set('display_errors', 1);
 
 $name = $_POST['name'];
 $password = $_POST['password'];
-$password = md5($password);
+$password = md5($password);     //encryption
 
 $erreturn_value="username=".$username;
 
-$results=$mysqli->query("SELECT * from user where username ='".$username."'");
+$results=$mysqli->query("SELECT * from user where username ='".$username."'");      //getting user details
 $obj=$results->fetch_object();
 $pass=$obj->password;
 $named =$obj-> name;
 
 if($password==$pass) {
-    if ($name!=$named){
+    if ($name!=$named){     //name change
         $query=$mysqli->query("Update user set name='$name' WHERE username='$username'");
         if($query==true){
-            echo '<meta http-equiv="refresh" content="0; URL=editdetails.php?success=saved">';
+            echo '<meta http-equiv="refresh" content="0; URL=editdetails.php?success=saved">';      //name changed
         } else {
-            echo '<meta http-equiv="refresh" content="0; URL=editdetails.php?databaseError;">';
+            echo '<meta http-equiv="refresh" content="0; URL=editdetails.php?databaseError;">';     //database error
         }
     } else {
-        echo '<meta http-equiv="refresh" content="0; URL=editdetails.php?err=duplicate&'.$erreturn_value.'"">';
+        echo '<meta http-equiv="refresh" content="0; URL=editdetails.php?err=duplicate&'.$erreturn_value.'"">';     //no change
     }
 }else
 {
-    echo '<meta http-equiv="refresh" content="0; URL=editdetails.php?err=passMissMatch&'.$erreturn_value.'"">';
+    echo '<meta http-equiv="refresh" content="0; URL=editdetails.php?err=passMissMatch&'.$erreturn_value.'"">';     //wrong password
 }
 ?>
