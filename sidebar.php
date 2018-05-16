b444219a43b4c8b2af7894afc494c90969100c72
vahesan1995 <vahesan.15@cse.mrt.ac.lk>
2018-05-16 22:14:09 +0530

    download icon added

--- sidebar.php
+++ sidebar.php
@@ -10,6 +10,7 @@
     <title>Title</title>
 </head>
 <body>
+<!--Sidebar contents starts here-->
 <div class="sidebar-menu">
     <header class="logo1">
         <a href="#" class="sidebar-icon"> <span class="fa fa-bars"></span> </a>
@@ -17,13 +18,12 @@
     <div style="border-top:1px ridge rgba(255, 255, 255, 0.15)"></div>
     <div class="menu">
         <ul id="menu" >
-
-            <li><a href="home.php"><i class="fa fa-home"></i> <span>HOME</span><div class="clearfix"></div></a></li>
-            <li ><a href="viewdetails.php"><i class="fa fa-file-text-o" aria-hidden="true"></i><span> VIEW DETAILS</span><div class="clearfix"></div></a></li>
-            <li ><a href="editdetails.php"><i class="fa fa-edit" aria-hidden="true"></i><span> CHANGE DETAILS</span><div class="clearfix"></div></a></li>
-            <li ><a href="viewFiles.php"><i class="fa fa-file-pdf-o" aria-hidden="true"></i><span> VIEW RESULTS </span> <div class="clearfix"></div></a></li>
-            <li ><a href="changepassword.php"><i class="fa fa-key" aria-hidden="true"></i><span> CHANGE PASSWORD </span> <div class="clearfix"></div></a></li>
-            <li ><a href="logout.php"><i class="fa fa-sign-out" aria-hidden="true"></i><span> LOGOUT</span> <div class="clearfix"></div></a></li>
+            <li><a href="home.php" id="home"><i class="fa fa-home"></i> <span>HOME</span><div class="clearfix"></div></a></li>
+            <li ><a href="viewdetails.php" id="viewdetails"><i class="fa fa-file-text-o" aria-hidden="true"></i><span> VIEW DETAILS</span><div class="clearfix"></div></a></li>
+            <li ><a href="editdetails.php" id="editdetails"><i class="fa fa-edit" aria-hidden="true"></i><span> CHANGE DETAILS</span><div class="clearfix"></div></a></li>
+            <li ><a href="viewFiles.php" id="viewfiles"><i class="fa fa-file-pdf-o" aria-hidden="true"></i><span> VIEW RESULTS </span> <div class="clearfix"></div></a></li>
+            <li ><a href="changepassword.php" id="changepassword"><i class="fa fa-key" aria-hidden="true"></i><span> CHANGE PASSWORD </span> <div class="clearfix"></div></a></li>
+            <li ><a href="logout.php" id="logout"><i class="fa fa-sign-out" aria-hidden="true"></i><span> LOGOUT</span> <div class="clearfix"></div></a></li>
         </ul>
     </div>
 </div>
