42b23fe288632fc246036259eeae4f19ded34c75
vahesan1995 <vahesan.15@cse.mrt.ac.lk>
2018-05-17 00:25:11 +0530

    minor changes

--- notifications.php
+++ notifications.php
@@ -1,7 +1,9 @@
 <?php
-include "config.php";
+include_once ("config.php");
+include_once('system_session.php');
+$username = $_SESSION['username'];  //get the username of the user
 
-$notiquery = "SELECT count(fileid) as count from files where notification=1";
+$notiquery = "SELECT count(fileid) as count from files where notification=1 and username='$username'"; //count=num of new files
 $notiquery =mysqli_query($mysqli,$notiquery);
 $result=mysqli_fetch_array($notiquery);
-$count=$result['count'];
+$count=$result['count'];    //new files count
