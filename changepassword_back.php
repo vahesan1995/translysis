2b7491a4e50f99ac58f6212dc22c27922a6d8316
vahesan1995 <vahesan.15@cse.mrt.ac.lk>
2018-05-17 03:20:24 +0530

    small edits

--- changepassword_back.php
+++ changepassword_back.php
@@ -1,39 +1,37 @@
 <?php
 include_once("config.php");
 include_once ("system_session.php");
-$username = $_SESSION['username'];
+$username = $_SESSION['username'];  //username of current user
 
-error_reporting(E_ALL);
+error_reporting(E_ALL); //reporting errors to front end
 ini_set('display_errors', 1);
-
-$password = $_POST['password'];
-$password = md5($password);
+$password = $_POST['password'];     //password obtained
+$password = md5($password);     //encryption
 
 $newpassword = $_POST['newpassword'];
-$newpassword = md5($newpassword);
+$newpassword = md5($newpassword);       //encryption
 $conpassword = $_POST['conpassword'];
-$conpassword = md5($conpassword);
+$conpassword = md5($conpassword);       //encryption
 
 $erreturn_value="username=".$username;
 
-$results=$mysqli->query("SELECT * from user where username ='".$username."'");
+$results=$mysqli->query("SELECT * from user where username ='".$username."'");      //getting details from user table
 $obj=$results->fetch_object();
 $pass=$obj->password;
-$named =$obj-> name;
 
 if($password==$pass) {
     if ($newpassword==$conpassword){
         $query=$mysqli->query("Update user set password='$newpassword' WHERE username='$username'");
         if($query==true){
-            echo '<meta http-equiv="refresh" content="0; URL=changepassword.php?success=saved">';
+            echo '<meta http-equiv="refresh" content="0; URL=changepassword.php?success=saved">';   //pasword changed
         } else {
-            echo '<meta http-equiv="refresh" content="0; URL=changepassword.php?databaseError;">';
+            echo '<meta http-equiv="refresh" content="0; URL=changepassword.php?databaseError;">';  //unable to save in database
         }
     } else {
-        echo '<meta http-equiv="refresh" content="0; URL=changepassword.php?err=duplicate&'.$erreturn_value.'"">';
+        echo '<meta http-equiv="refresh" content="0; URL=changepassword.php?err=duplicate&'.$erreturn_value.'"">';  //new passwords mismatch
     }
 }else
 {
-    echo '<meta http-equiv="refresh" content="0; URL=changepassword.php?err=passMissMatch&'.$erreturn_value.'"">';
+    echo '<meta http-equiv="refresh" content="0; URL=changepassword.php?err=passMissMatch&'.$erreturn_value.'"">';  //incorrect password
 }
 ?>
