64a976547480d4c871a7fb512b7b799a1364f508
vahesan1995 <vahesan.15@cse.mrt.ac.lk>
2018-05-17 03:59:42 +0530

    small changes

--- logout.php
+++ logout.php
@@ -3,12 +3,13 @@ ob_start();
 session_start();
 if(isset($_SESSION['user_login_status']))
 {
+    //unset all the session details
     unset($_SESSION['user_login_status']);
     unset($_SESSION['userId']);
     unset($_SESSION['position']);
     unset($_SESSION['username']);
     //session_destroy();
-    header("Location: index.php");
+    header("Location: index.php");  //redirect to login page
 }
 else
 {
