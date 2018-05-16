e5ce0d1ece9d6bd98e774ad8e4892ee3012eb5e1
vahesan1995 <vahesan.15@cse.mrt.ac.lk>
2018-05-17 03:58:11 +0530

    small changes

--- loginSessionCheck.php
+++ loginSessionCheck.php
@@ -3,9 +3,9 @@ ob_start();
 session_start();
 if(isset($_SESSION['user_login_status']))
 {
-    if($_SESSION['user_login_status']=="alreadylogedin")
+    if($_SESSION['user_login_status']=="alreadylogedin")        //check whether user is already signed in
     {
-        header("location:index.php");
+        header("location:index.php");       //redirect to home
     }
 }
 ?>
